6657ebff2ac4f6067993fc3812d6e5cd830381c4
sakshamjakhar <sakshamjakhar008800@gmail.com>
2025-07-17 10:35:30 +0530

    Added a new JS file in JS branch

---
+++ Practice.js
@@ -0,0 +1,16 @@
+let a = 13;
+let b = 4;
+
+let sum = a + b;
+let difference = a - b;
+let Multiply = a * b;
+let Divide = a / b;
+console.log("Sum", sum);
+console.log("Difference=", difference);
+
+console.log("Multiplication=", Multiply);
+console.log("Division=", Divide);
+console.log("Modulus=", a % b);
+console.log("Exponentiation=", a ** b);
+console.log("Increment=", ++a);
+console.log("Decrement=", --b); 
